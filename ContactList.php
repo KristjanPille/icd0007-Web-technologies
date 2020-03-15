8381f02e07f7b16f3c813d779de780a99217669a
krist <krpill@ttu.ee>
2020-03-15 18:29:40 +0200

    homework 3

--- ContactList.php
+++ ContactList.php
@@ -5,14 +5,14 @@ const DATA_FILE = "data.txt";
 
 function addContact($contactItem){
     $serializeditem = "$contactItem->firstName**$contactItem->lastName**$contactItem->phone";
-    file_put_contents(DATA_FILE, $serializeditem . PHP_EOL, FILE_APPEND);
+    file_put_contents(urlencode(DATA_FILE), $serializeditem . PHP_EOL, FILE_APPEND);
 }
 
 function getContactItems(){
     $lines = file(DATA_FILE, FILE_IGNORE_NEW_LINES);
     $contactItems = [];
     foreach ($lines as $line){
-        list($firstName, $lastName, $phone) = explode("**", $line);
+        list($firstName, $lastName, $phone) = explode("**", urldecode($line));
         $contactItem = new contactItem($firstName, $lastName, $phone);
         $contactItems[] = $contactItem;
     }

53a665d8827c903361c0f7e9bea63906de413d08
krpill <krpill@ttu.ee>
2020-04-21 15:23:20 +0300

    hw6 done, design todo

--- contactItem.php
+++ contactItem.php
@@ -4,13 +4,15 @@ class contactItem{
     public $firstName;
     public $lastName;
     public $phone;
+    public $id;
 
-    public function __construct($firstName, $lastName, $phone) {
+    public function __construct($firstName, $lastName, $phone, $id = null) {
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->phone = $phone;
+        $this->id = $id;
     }
     public function __toString() {
-        return "item{id: $this->firstName, name: $this->lastName, lastName: $this->phone}";
+        return "item{id: $this->id, firstname: $this->firstName, lastName: $this->lastName, phone: $this->phone}";
     }
 }

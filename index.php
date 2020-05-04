91140f8f42700fe683475794c888889e4b1e78fd
krpill <krpill@ttu.ee>
2020-05-04 14:33:23 +0300

    hw7 0.3

--- index.php
+++ index.php
@@ -32,6 +32,13 @@ if ($cmd === "login_page") {
 if ($cmd === "log_out") {
     header( "Location: /?cmd=login_page" );
     $_SESSION = array();
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
     session_destroy();
     exit;
 }
